8efd7645f457f392889696db5fa2366205d1b1cf
hzrd149 <github@hzrd149.com>
2025-10-02 15:58:27 +0100

    fix bundle build

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import { IndexedDBNostrDB } from "./nostrdb-idb.js";
 export * from "./interface.js";
 export * from "./nostrdb-idb.js";
 
-if (typeof window !== "undefined" && !("nostrdb" in window))
+if (typeof window !== "undefined" && !("nostrdb" in window)) {
   // @ts-expect-error
   window.nostrdb = new IndexedDBNostrDB();
+  console.log("polyfilled window.nostrdb");
+}
